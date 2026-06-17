c601b52fa2de5cf675f402e51f2c1abf340ef6df
Dominik <easyflowautomation.ai@gmail.com>
2026-06-17 17:43:30 +0200

    correct project structure

---
+++ app/app/page.tsx
@@ -0,0 +1,28 @@
+export default function AppPage() {
+  return (
+    <div style={{
+      fontFamily: "sans-serif",
+      textAlign: "center",
+      padding: "60px"
+    }}>
+      <h1>KeepPushing Running Coach</h1>
+
+      <p>Dein Trainingsplan für bessere Läufe</p>
+
+      <a
+        href="https://apps.apple.com/app/id6755646753"
+        style={{
+          display: "inline-block",
+          marginTop: "20px",
+          padding: "15px 25px",
+          background: "black",
+          color: "white",
+          borderRadius: "10px",
+          textDecoration: "none"
+        }}
+      >
+        Download iOS App
+      </a>
+    </div>
+  );
+}
